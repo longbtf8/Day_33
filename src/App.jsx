f9dbf63f0e96ba671c44b028624410ae3095ad11
longbtf8 <longbtf8@fullstack.edu.vn>
2025-11-03 22:24:39 +0700

    Fix base path for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 function App() {
   return (
-    <Router>
+    <Router basename="/Day_33">
       <>
         <Header />
         <main>
